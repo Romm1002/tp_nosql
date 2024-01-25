5778aacaf34fd6bfa5cf0c2f329e31a1c9962a33
zbrwkq <damien.chv78@gmail.com>
2024-01-25 16:49:38 +0100

    details

--- app.py
+++ app.py
@@ -67,6 +67,26 @@ elif selected_page == "Genres":
     st.write("---")
        
 elif selected_page == "Graphiques":
+    
+    # 
+    # GRAPHIQUE DES ARTISTES LES PLUS POPULAIRES
+    # 
+    top_artists = pd.DataFrame(list(collection_artists.find({}, {"_id": 0, "name": 1, "popularity": 1, "genres": 1})))
+    top_5_artists = top_artists.nlargest(5, 'popularity')
+    st.subheader("Les 5 artistes les plus populaires")
+    fig, ax = plt.subplots()
+    top_5_artists.plot(kind='bar', x='name', y='popularity', ax=ax, rot=45)
+    ax.set_xlabel("Artiste")
+    ax.set_ylabel("Popularité")
+    ax.set_title("Les 5 artistes les plus populaires")
+    st.pyplot(fig)
+
+    st.subheader("Genres des 5 artistes les plus populaires")
+    for index, artist in top_5_artists.iterrows() :
+        name = artist["name"]
+        genres = ', '.join(artist["genres"])
+        st.write(f"{name} - {genres}")
+
     # 
     # GRAPHIQUE DU NOMBRE D'ARTISTES PAR GENRES
     # 
@@ -99,41 +119,39 @@ elif selected_page == "Graphiques":
     ax.set_title("Nombre d'artistes par genre musical (Top 5)")
     st.pyplot(fig)
 
-
-    # 
-    # GRAPHIQUE DES ARTISTES LES PLUS POPULAIRES
-    # 
-    top_artists = pd.DataFrame(list(collection_artists.find({}, {"_id": 0, "name": 1, "popularity": 1})))
-    top_5_artists = top_artists.nlargest(5, 'popularity')
-
-    st.subheader("Les 5 artistes les plus populaires")
-    fig, ax = plt.subplots()
-    top_5_artists.plot(kind='bar', x='name', y='popularity', ax=ax, rot=45)
-    ax.set_xlabel("Artiste")
-    ax.set_ylabel("Popularité")
-    ax.set_title("Les 5 artistes les plus populaires")
-    st.pyplot(fig)
-
-
-    st.write("---")
     # 
     # GRAPHIQUE REPARTITION DE LA POPULARTIE DES ARTISTES PAR GENRE
     #
-    genres_data = {genre["nom"]: genre["_id"] for genre in collection_genres.find().limit(40)}
+    st.subheader("Répartition des artistes populaires entre les genres")
+
+    genres_data = {genre["nom"]: genre["_id"] for genre in collection_genres.find()}
     genres = genres = list(genres_data.keys())
 
     selected_genre_name = st.selectbox("Sélectionnez un genre", genres)
     selected_genre_id = genres_data[selected_genre_name]
     if selected_genre_id:
-        artists_document = db['artists.artists_genres'].aggregate([
+        artists_popularity = pd.DataFrame(db['artists.artists_genres'].aggregate([
             {"$match": {"genre_id":  selected_genre_id}},
             {"$lookup": {"from": "artists", "localField": "artist_id", "foreignField": "_id", "as": "artist"}},
             {"$unwind": "$artist"},
-            {"$sort": {"artist.popularity": -1}}
-        ])
+            {"$group": {
+                "_id": "$artist.popularity",
+                "count": {"$sum": 1}
+            }},
+            {"$sort": {"_id": -1}}
+        ]))
 
-        data = [{"_id": doc['artist']['_id'], "popularity": doc['artist']['popularity']} for doc in artists_document]
-        st.write(f"{len(data)} artistes dans ce genre")
+        count = artists_popularity["count"].sum()
+        st.write(f"{count} artistes dans ce genre")
+
+        
+        fig, ax = plt.subplots()
+        artists_popularity.sort_values(by='_id', ascending=True).plot(kind='bar', x='_id', y='count', ax=ax, rot=45)
+        ax.set_xlabel("Popularité")
+        ax.set_ylabel("Artistes")
+        ax.set_title("Nombre d'artiste par popularité")
+        st.pyplot(fig)
+
 
 
 else:
